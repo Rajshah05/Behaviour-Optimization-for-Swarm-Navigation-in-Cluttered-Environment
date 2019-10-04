8372082f92a9bd2cd7c953267255dad479b26a88
Chris Gnam <crgnam@buffalo.edu>
2019-10-04 16:41:55 -0400

    added outline to abstract, and added quiver overlay for applied actuations

--- stable_flocking_implementation/RUNME.m
+++ stable_flocking_implementation/RUNME.m
@@ -6,7 +6,7 @@ rng(1); %(random seed for repeatability)
 
 %% Settings:
 % Basic Simulation Settings:
-write_to_video = true;
+write_to_video = false;
 dt = 1;
 tspan = 0:dt:300;
 WIDTH = 150; %(half width of the field)
@@ -80,11 +80,11 @@ end
 %% Create flock animation:
 if write_to_video
     
-   vid = VideoWriter('flock_animation.avi','Uncompressed AVI'); 
+   vid = VideoWriter('flock_animation.avi'); 
    open(vid)
 end
 
-figure(1)
+figure('units','normalized','outerposition',[0 0 1 1])
     % Initialize the animation:
     jj = 1;
     virtual_leader = plot(vl_rv(1,1),vl_rv(2,1),'sr','MarkerSize',10,'MarkerFaceColor','r'); hold on
@@ -94,9 +94,13 @@ figure(1)
     end
     agents = gobjects(num_agents,1);
     for ii = 1:2:2*num_agents
-        agents(jj) = plot(r(ii,1),r(ii+1,1),'.','MarkerSize',20);
+        agents(jj) = plot(r(ii,1),r(ii+1,1),'.k','MarkerSize',20);
         jj = jj+1;
     end
+    r_vec = reshape(r(:,ii)',2,[])';
+    u_vec = reshape(u(:,ii)',2,[])';
+    [~,u_norms] = normr(u_vec);
+    actuations = quiver(r_vec(:,1),r_vec(:,2),u_vec(:,1),u_vec(:,2),'color','r');
     axis equal
     grid on
     legend([virtual_leader, agents(1),obstacles(1)],...
@@ -107,10 +111,13 @@ figure(1)
     for ii = 1:L
         % Add track history:
         r_vec = reshape(r(:,ii)',2,[])';
-        plot(r_vec(:,1),r_vec(:,2),'.','color',[.5 .5 .5],'MarkerSize',2); hold on
+        u_vec = reshape(u(:,ii)',2,[])';
+        plot(r_vec(:,1),r_vec(:,2),'.','color',[.5 .5 .5],'MarkerSize',5); hold on
         for jj = 1:num_agents
             set(agents(jj),'XData',r_vec(jj,1),'YData',r_vec(jj,2));
         end
+        set(actuations,'XData',r_vec(:,1),'YData',r_vec(:,2),...
+                       'UData',max_u./u_vec(:,1),'VData',max_u./u_vec(:,2));
         set(virtual_leader,'XData',vl_rv(1,ii),'YData',vl_rv(2,ii));
         xlim([-WIDTH/2 1.5*WIDTH])
         ylim([-WIDTH/2 1.5*WIDTH])
