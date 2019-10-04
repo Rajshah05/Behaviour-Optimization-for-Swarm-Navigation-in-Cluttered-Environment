99ad88798aebb406f3595d9d5c8f20b63ce876a6
Chris Gnam <crgnam@buffalo.edu>
2019-10-04 15:00:40 -0400

    simple virtual leader follower contorller working!

--- stable_flocking_implementation/RUNME.m
+++ stable_flocking_implementation/RUNME.m
@@ -5,33 +5,44 @@ addpath(genpath('tools'))
 
 % Time settings:
 dt = 1;
-duration = 60;
+duration = 600;
 tspan = 0:dt:duration;
 
 % Agent definitions:
-num_agents = 5;
+num_agents = 6;
+
+% Virtual leader:
+vl = [-30 30 .25 -.75]; %(rx,ry,vx,vy)
+d0 = 20;
+
+% Obstacles:
+num_obs = 1;
+obs = [30 20 10];
 
 % Memory allocation:
 L = length(tspan);
 u = zeros(2*num_agents,L);
 r = zeros(2*num_agents,L);
 v = zeros(2*num_agents,L);
+vl_rv = zeros(4,L);
 
 % Initial conditions
 r(:,1) = 20*randn(2*num_agents,1);
 v(:,1) = 1*randn(2*num_agents,1);
+vl_rv(:,1) = vl';
 
 max_v = 5;
-max_u = 1e-1;
+max_u = 1;
 
 for ii = 1:L-1
     % Propagate the dynamics:
     X_out = RK4(@equations_of_motion,dt,[r(:,ii);v(:,ii)],u(:,ii));
     r(:,ii+1) = X_out(1:2*num_agents);
-    v(:,ii+1) = X_out(2*num_agents+1:end);
+    v(:,ii+1) = X_out(2*num_agents+1:4*num_agents);
+    vl_rv(:,ii+1) = RK4(@equations_of_motion,dt,vl_rv(:,ii),[0;0]);
     
     % Calculate the control:
-    u(:,ii+1) = controller(r(:,ii+1),v(:,ii+1));
+    u(:,ii+1) = controller(r(:,ii+1),v(:,ii+1),vl_rv(:,ii+1)',d0);
     
     % Apply limitations on control input:
     u_vec = reshape(u(:,ii+1)',2,[])';
@@ -48,60 +59,75 @@ for ii = 1:L-1
 end
 
 % Create flock animation:
-% figure()
-%     % Initialize the animation:
-%     for ii = 1:2:2*num_agents
-%         agents(ii) = plot(r(ii,1),r(ii+1,1),'.','MarkerSize',20); hold on
-% %         controller(ii)
-%     end
-%     for ii = 1:L
-%         % Add track history:
-%         r_vec = reshape(r(:,ii)',2,[])';
-%         plot(r_vec(:,1),r_vec(:,2),'.','color',[.5 .5 .5],'MarkerSize',15); hold on
-%         
-%         xlim([-100 100])
-%         ylim([-100 100])
-%         drawnow
-%         pause(.1)
-%     end
+figure()
+    % Initialize the animation:
+    jj = 1;
+    virtual_leader = plot(vl_rv(1,1),vl_rv(2,1),'sr','MarkerSize',10,'MarkerFaceColor','r'); hold on
+    obstacles = gobjects(num_obs,1);
+    for ii = 1:num_obs
+        obstacles(ii) = circle(obs(ii,1),obs(ii,2),obs(ii,3));
+    end
+    agents = gobjects(num_agents,1);
+    for ii = 1:2:2*num_agents
+        agents(jj) = plot(r(ii,1),r(ii+1,1),'.','MarkerSize',20);
+        jj = jj+1;
+    end
+    axis equal
+    grid on
+    legend([virtual_leader, agents(1),obstacles(1)],'Virtual Leader','Agents','Obstacle')
+    
+    % Actuall show the animation:
+    for ii = 1:L
+        % Add track history:
+        r_vec = reshape(r(:,ii)',2,[])';
+        plot(r_vec(:,1),r_vec(:,2),'.','color',[.5 .5 .5],'MarkerSize',2); hold on
+        for jj = 1:num_agents
+            set(agents(jj),'XData',r_vec(jj,1),'YData',r_vec(jj,2));
+        end
+        set(virtual_leader,'XData',vl_rv(1,ii),'YData',vl_rv(2,ii));
+        xlim([-100 100])
+        ylim([-100 100])
+        drawnow
+        pause(.1)
+    end
 
 % Plot summary of the results:
-figure()
-    subplot(2,3,1)
-        for ii = 1:2:2*num_agents
-            plot(tspan,r(ii,:)); hold on
-        end
-        grid on
-        title('R_x')
-    subplot(2,3,4)
-        for ii = 1:2:2*num_agents
-            plot(tspan,r(ii+1,:)); hold on
-        end
-        grid on
-        title('R_y')
-        
-    subplot(2,3,2)
-        for ii = 1:2:2*num_agents
-            plot(tspan,v(ii,:)); hold on
-        end
-        grid on
-        title('V_x')
-    subplot(2,3,5)
-        for ii = 1:2:2*num_agents
-            plot(tspan,v(ii+1,:)); hold on
-        end
-        grid on
-        title('V_y')
-        
-    subplot(2,3,3)
-        for ii = 1:2:2*num_agents
-            plot(tspan,u(ii,:)); hold on
-        end
-        grid on
-        title('U_x')
-    subplot(2,3,6)
-        for ii = 1:2:2*num_agents
-            plot(tspan,u(ii+1,:)); hold on
-        end
-        grid on
-        title('U_y')
+% figure()
+%     subplot(2,3,1)
+%         for ii = 1:2:2*num_agents
+%             plot(tspan,r(ii,:)); hold on
+%         end
+%         grid on
+%         title('R_x')
+%     subplot(2,3,4)
+%         for ii = 1:2:2*num_agents
+%             plot(tspan,r(ii+1,:)); hold on
+%         end
+%         grid on
+%         title('R_y')
+%         
+%     subplot(2,3,2)
+%         for ii = 1:2:2*num_agents
+%             plot(tspan,v(ii,:)); hold on
+%         end
+%         grid on
+%         title('V_x')
+%     subplot(2,3,5)
+%         for ii = 1:2:2*num_agents
+%             plot(tspan,v(ii+1,:)); hold on
+%         end
+%         grid on
+%         title('V_y')
+%         
+%     subplot(2,3,3)
+%         for ii = 1:2:2*num_agents
+%             plot(tspan,u(ii,:)); hold on
+%         end
+%         grid on
+%         title('U_x')
+%     subplot(2,3,6)
+%         for ii = 1:2:2*num_agents
+%             plot(tspan,u(ii+1,:)); hold on
+%         end
+%         grid on
+%         title('U_y')
--- stable_flocking_implementation/controllers/controller.m
+++ stable_flocking_implementation/controllers/controller.m
@@ -1,4 +1,4 @@
-function [u] = controller(r,v)
+function [u] = controller(r,v,vl,d0)
     % Actual controller implementation:
     num_agents = length(r)/2;
     
@@ -11,11 +11,25 @@ function [u] = controller(r,v)
     
     % Control law:
     u = zeros(num_agents,2);
-    kp_v = .1;
-    kp_grad = .1;
+    
+    % Control gains:
+    k_ria = 10; %(inter-agent position)
+    k_via = .1;  %(inter-agent velocities)
+    k_rvl = .25; %(virtual-leader position)
+    k_vvl = .5; %(virtual-leader velocity)
+    [vl_dirs,vl_d] = normr(r_vec - vl(1:2));
+    vl_dx = vl_d - d0;
     for ii = 1:num_agents
-       u(ii,:) = -kp_v*sum(v_vec(ii,:) - v_vec([1:ii-1,ii+1:end],:),1) + ...
-                 -kp_grad*sum(0);
+        % Formulate error signals:
+        [ia_dirs,ia_d] = normr(r_vec(ii,:) - r_vec([1:ii-1,ii+1:end],:));
+        e_ria = sum(ia_dirs./ia_d);
+        e_via = sum(v_vec(ii,:) - v_vec([1:ii-1,ii+1:end],:),1);
+        e_rvl = vl_dirs(ii,:)*vl_dx(ii);
+        e_vvl = sum(v_vec(ii,:) - vl(3:4),1);
+        
+        % Apply gains:
+        u(ii,:) = k_ria*e_ria -k_via*e_via -k_rvl*e_rvl -k_vvl*e_vvl;
+                  
     end
     
     % Reformulate:
